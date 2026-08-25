4418842d8f472891fce169143abd2886b2ee8925
Sean Cheema <92892168+seankcheema@users.noreply.github.com>
2026-08-25 08:58:06 -0500

    fix: grammar check

--- src/main/java/com/neueda/leap/Main.java
+++ src/main/java/com/neueda/leap/Main.java
@@ -2,6 +2,6 @@ package com.neueda.leap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world from PR Lab! Mohammeds Shoaib and Sean's changes");
+        System.out.println("Hello world from PR Lab! Mohammed Shoaib and Sean's changes");
     }
 }
